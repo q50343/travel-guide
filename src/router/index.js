6f8210397e56859caf6e2837c8dc4ec010d44f13
LAPTOP-404IIU9E\yuhan <yuhan.gao@timecitytech.com>
2022-03-09 05:16:22 +0800

    route

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Detail from './../views/Detail.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {path: '/'},
     { path: '/', name: 'Home', component: Home },
